e5125b489db0090b4fb7a932882be15fbec9e05c
mvciek <mpeciakk@gmail.com>
2021-10-26 19:01:12 +0200

    simple authentication using token

--- src/app.ts
+++ src/app.ts
@@ -4,16 +4,30 @@ import bodyParser from 'koa-bodyparser'
 import pm2 from 'pm2'
 import { promisify } from 'util'
 import { freemem, totalmem } from 'os'
+import dotenv from 'dotenv'
 
 const PORT = 3030
 
 const app = new Koa()
 const router = new Router()
 
+dotenv.config()
+
 pm2.connect((err) => {
     if (err) {
         console.error(err)
         process.exit(2)
+    }
+})
+
+app.use(async (ctx, next) => {
+    if (!ctx.header.authorization || ctx.header.authorization.split(' ')[1] != process.env.TOKEN) {
+        ctx.body = {
+            status: 401,
+            message: 'Unauthorized',
+        }
+    } else {
+        await next()
     }
 })
 
